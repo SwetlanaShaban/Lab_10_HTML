dba4594e4360dd602c123c8eeed9671398239612
Андрей <andrey.naumovich@mail.ru>
2018-05-11 22:43:57 +0300

    Question class

---
+++ TestAnswerDispenser/TestDispenser/Controllers/AnswersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestDispenser.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/answers")]
+    public class AnswersController : Controller
+    {
+        // GET: api/answers
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/answers/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
---
+++ TestAnswerDispenser/TestDispenser/Model/Question.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TestDispenser.Model
+{
+    public class Question
+    {
+        private readonly string _questionText;
+        private readonly IDictionary<int, string> _variants;
+        private readonly int _correct;
+
+        public Question(string questionText, int correct, params string[] variants)
+        {
+            if (string.IsNullOrEmpty(questionText) || correct <= 0 || correct > variants.Length || variants.Length <= 1)
+                throw new ArgumentException("Check question params in config file");
+
+            _questionText = questionText;
+            _correct = correct;
+            _variants = new Dictionary<int, string>();
+
+            int i = 1;
+            foreach (var variant in variants)
+            {
+                _variants.Add(i++, variant);
+            }
+        }
+
+        [JsonProperty]
+        private string QuestionText => _questionText;
+
+        [JsonProperty]
+        private IDictionary<int, string> Variants => _variants;
+
+        [JsonProperty]
+        private int Correct => _correct;
+    }
+}
